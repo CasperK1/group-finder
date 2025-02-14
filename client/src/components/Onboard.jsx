ec6b9079b485ede0975398001936f9604e81b72a
PhongLe7de <phongle260260@gmail.com>
2025-02-14 11:44:05 +0200

    solve-leevi-branch

--- client/src/components/Onboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Onboard() {
-  return (
-    <div>Onboard</div>
-  )
-}
-
-export default Onboard
---
+++ client/src/data/GroupsData.jsx
@@ -0,0 +1,14 @@
+const groupsData = [
+    { id: 1, name: "Group 1", category: "Software Engineering", icon: "📘" },
+    { id: 2, name: "Group 2", category: "Software Engineering", icon: "📘" },
+    { id: 3, name: "Group 3", category: "Health Tech", icon: "🔤" },
+    { id: 4, name: "Group 4", category: "Software Engineering", icon: "🎯" },
+    { id: 5, name: "Group 5", category: "Software Engineering", icon: "🌐" },
+    { id: 6, name: "Group 6", category: "Software Engineering", icon: "🏠" },
+    { id: 7, name: "Group 7", category: "Health Tech", icon: "📒" },
+    { id: 8, name: "Group 8", category: "Health Tech", icon: "➕" },
+    { id: 9, name: "Group 9", category: "Software Engineering", icon: "📘" }
+  ];
+  
+  export default groupsData;
+  
